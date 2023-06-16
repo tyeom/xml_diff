41898cc960132f0723a23b3736a73aae2ac00e6e
tyeom <r.Until@gmail.com>
2023-06-16 13:51:21 +0900

    .

---
+++ XmlDiffLib/Behaviors/TreeViewItemMouseOverBehavior.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace XmlDiffLib.Behaviors
+{
+    public static class TreeViewItemMouseOverBehavior
+    {
+        private static TreeViewItem _currentItem = null;
+
+        static TreeViewItemMouseOverBehavior()
+        {
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), TreeViewItem.MouseEnterEvent, new MouseEventHandler(OnMouseTransition), true);
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), TreeViewItem.MouseLeaveEvent, new MouseEventHandler(OnMouseTransition), true);
+
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), UpdateOverItemEvent, new RoutedEventHandler(OnUpdateOverItem));
+        }
+
+        private static readonly DependencyPropertyKey IsMouseDirectlyOverItemKey =
+            DependencyProperty.RegisterAttachedReadOnly("IsMouseDirectlyOverItem",
+                                                typeof(bool),
+                                                typeof(TreeViewItemMouseOverBehavior),
+                                                new FrameworkPropertyMetadata(null, new CoerceValueCallback(CalculateIsMouseDirectlyOverItem)));
+
+        public static readonly DependencyProperty IsMouseDirectlyOverItemProperty =
+            IsMouseDirectlyOverItemKey.DependencyProperty;
+
+        public static bool GetIsMouseDirectlyOverItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMouseDirectlyOverItemProperty);
+        }
+
+        private static object CalculateIsMouseDirectlyOverItem(DependencyObject item, object value)
+        {
+            if (item == _currentItem)
+                return true;
+            else
+                return false;
+        }
+
+        private static readonly RoutedEvent UpdateOverItemEvent = EventManager.RegisterRoutedEvent(
+            "UpdateOverItem", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TreeViewItemMouseOverBehavior));
+
+        static void OnUpdateOverItem(object sender, RoutedEventArgs args)
+        {
+            _currentItem = sender as TreeViewItem;
+            _currentItem.InvalidateProperty(IsMouseDirectlyOverItemProperty);
+
+            args.Handled = true;
+        }
+
+        static void OnMouseTransition(object sender, MouseEventArgs args)
+        {
+            lock (IsMouseDirectlyOverItemProperty)
+            {
+                if (_currentItem != null)
+                {
+                    DependencyObject oldItem = _currentItem;
+                    _currentItem = null;
+                    oldItem.InvalidateProperty(IsMouseDirectlyOverItemProperty);
+                }
+
+                IInputElement currentPosition = Mouse.DirectlyOver;
+
+                if (currentPosition != null)
+                {
+                    RoutedEventArgs newItemArgs = new RoutedEventArgs(UpdateOverItemEvent);
+                    currentPosition.RaiseEvent(newItemArgs);
+
+                }
+            }
+        }
+    }
+}
